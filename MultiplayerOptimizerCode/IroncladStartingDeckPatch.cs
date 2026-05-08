ec6582693c59b7034606af888868ad7a007bef29
bwnotfound <2846131442@qq.com>
2026-05-09 01:14:50 +0800

    update: 添加测试

---
+++ MultiplayerOptimizerCode/IroncladStartingDeckPatch.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using HarmonyLib;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.Characters;
+
+namespace MultiplayerOptimizer.MultiplayerOptimizerCode;
+
+[HarmonyPatch(typeof(Ironclad), nameof(Ironclad.StartingDeck), MethodType.Getter)]
+public static class IroncladStartingDeckPatch
+{
+    public static void Postfix(ref IEnumerable<CardModel> __result)
+    {
+        CardModel testCard = ModelDb.Card<TestCard>();
+
+        __result = __result.Concat([testCard]);
+
+        Godot.GD.Print("[MultiplayerOptimizer] Patched Ironclad.StartingDeck: added TestCard.");
+    }
+}
